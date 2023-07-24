9b0eb8b5d457f849d38e74cdc7ff707374f3e5ec
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-07-24 01:06:50 -0500

    Add files via upload

---
+++ 150smth.py
@@ -0,0 +1,7 @@
+display_country_list["text"] = "Country Name :" + str(country_list)
+display_capital_list["text"] = "City Name :" + str(capital_list)
+random_no1 = random.randint(0,length1-1)
+random_country = country_list[random_no1]
+display_random_capital_list["text"] = "Random City : " + str(random_capital)
+random_no2 = random.rqandint(0,length2-1)
+random_capital = capital_list[random_no2]
